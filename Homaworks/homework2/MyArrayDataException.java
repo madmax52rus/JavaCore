2d2682001e1d7b41ad96eaa1e4929eeac35529ac
madmax52rus <88784403+madmax52rus@users.noreply.github.com>
2022-03-30 16:39:51 +0300

    Update MyArrayDataException.java

--- Homaworks/homework2/MyArrayDataException.java
+++ Homaworks/homework2/MyArrayDataException.java
@@ -1,6 +1,7 @@
 package lesson2.homework2;
 
 public class MyArrayDataException extends Exception {
-    public MyArrayDataException(int i, int j) {
+    public MyArrayDataException(String message) {
+        super(message);
     }
 }
