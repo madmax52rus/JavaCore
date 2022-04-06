07fafbe6dfdfbc7919d1110812a586791d2eaa21
madmax52rus <88784403+madmax52rus@users.noreply.github.com>
2022-04-06 16:46:58 +0300

    Update PhoneBook.java

--- Homaworks/homework4/PhoneBook.java
+++ Homaworks/homework4/PhoneBook.java
@@ -2,17 +2,30 @@ package homework4;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Hashtable;
 
 public class PhoneBook {
+
+   private HashMap<String, ArrayList<String>> phoneDirectory = new HashMap<>();
+
+   public void add(String name, String phone) {
+       ArrayList<String> abonentName = phoneDirectory.getOrDefault(name, new ArrayList<>());
+       abonentName.add(phone);
+       phoneDirectory.put(name, abonentName);
+   }
+
+   public ArrayList<String> get(String name) {
+       return phoneDirectory.get(name);
+   }
+
     public static void main(String[] args) {
+        PhoneBook phonebook = new PhoneBook();
+        phonebook.add("Max", "915999");
+        phonebook.add("Yaroslav", "910999");
+        phonebook.add("Max", "915988");
+        phonebook.add("Vyacheslav", "909999");
+        phonebook.add("Max", "987999");
 
-        HashMap<String, String> phoneDirectory = new HashMap<>();
-        phoneDirectory.put("Max", "9159995588");
-        phoneDirectory.put("Yaroslav", "9109995588");
-        phoneDirectory.put("Max", "9159991488");
-        phoneDirectory.put("Vyacheslav", "9099995588");
-        phoneDirectory.put("Max", "9879999988");
-        System.out.println(phoneDirectory);
+        System.out.println(phonebook.get("Max"));
     }
-
 }
